0da79b4a0f691b4e800d6dd499ad867143363f09
Marble-Frog <kadesamboateng@googlemail.com>
2024-03-04 12:03:24 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+funtction runGame() {
+
+}
+
+funtction checkAnswers() {
+
+}
+
+funtction calculateCorrectAnswer() {
+
+}
+
+funtction incrementScore() {
+
+}
+
+funtction incrementWrongAnswe() {
+
+}
+
+funtction displayAdditionQuestion() {
+
+}
+
+funtction displaySubtractionQuestion() {
+
+}
+
+funtction displayMultiplyQuestion() {
+
+}
