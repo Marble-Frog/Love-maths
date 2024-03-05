6ad723cbd9c59181161fd8421bd5499fd8fbd0b4
Marble-Frog <kadesamboateng@googlemail.com>
2024-03-05 20:19:16 +0000

    Connected the operands and operator to the calculate correct answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function(){
-    let buttons = docoument.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons){
         button.addEventListener("click", function() {
@@ -9,28 +9,52 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
+
 } )
 
 /**
  * The main game "loop", called when the script is first loaded 
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     // Creates two random Numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 24 ) + 1
     let num2 = Math.floor(Math.random() * 24 ) + 1
+
+    if  (gameType === "addition") {
+        displayAdditionQuestion(num1, num2)
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswers() {
 
 }
 
+/**
+ * Gets the operands (the number) and the operator (plus minus etc)
+ * directly form the dom and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator =  document.getElementById('operator').innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`
+    }
 
 }
 
@@ -42,8 +66,10 @@ function incrementWrongAnswe() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractionQuestion() {
@@ -51,5 +77,5 @@ function displaySubtractionQuestion() {
 }
 
 function displayMultiplyQuestion() {
-
+    
 }
